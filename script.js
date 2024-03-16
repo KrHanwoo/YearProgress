d419e8aa3f0f87137526a7d6d3287816d901bd19
KrHanwoo <koreahan8549@gmail.com>
2024-03-17 00:17:58 +0900

    update

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const win = document.getElementById('window');
 const header = document.getElementById('header');
+const headerTitle = document.getElementById('header-title');
+const textPercentage = document.getElementById('text-percentage');
 
 let pos = [0, 0, 0, 0];
 let visible = true;
 let innerFocus = true;
+
+(async () => {
+  let fromYear = document.getElementById('from-year');
+  let toYear = document.getElementById('to-year');
+  let yearNow = new Date().getFullYear();
+  let yearThen = yearNow + 1;
+  fromYear.textContent = `${yearNow}년`;
+  fromYear.href = `https://namu.wiki/w/${yearNow}%EB%85%84`;
+  toYear.textContent = `${yearThen}년`;
+  toYear.href = `https://namu.wiki/w/${yearThen}%EB%85%84`;
+
+  let diff = getStartOf(yearThen) - getStartOf(yearNow);
+  let current = Date.now() - getStartOf(yearNow);
+  let percentage = Math.floor(current / diff * 100);
+
+  headerTitle.textContent = `${percentage}% 완료`;
+  textPercentage.textContent = `${percentage}% 완료`;
+})();
+
+function getStartOf(year) {
+  let date = new Date();
+  date.setFullYear(year);
+  date.setMonth(0, 1);
+  date.setHours(0, 0, 0, 0);
+  return date.getTime();
+}
 
 function toggle() {
   visible = !visible;
